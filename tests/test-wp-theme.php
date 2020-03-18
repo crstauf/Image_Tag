3807aac30f80b8d3d0905cd9c84a380eba528034
Caleb Stauffer <develop+github@calebstauffer.com>
2020-03-17 23:09:09 -0400

    expand coverage

--- tests/test-wp-theme.php
+++ tests/test-wp-theme.php
@@ -6,27 +6,26 @@
  */
 class Image_Tag_WP_Theme_Test extends WP_UnitTestCase {
 
-	static $img;
 	const SRC = 'assets/images/2020-landscape-1.png';
 
-	static function setUpBeforeClass() {
-		static::$img = Image_Tag::create( static::SRC );
-	}
-
 	function test_source() {
-		$this->assertEquals( trailingslashit( get_stylesheet_directory_uri() ) . static::SRC, static::$img->get_attribute( 'src' ) );
+		$img = Image_Tag::create( static::SRC );
+		$this->assertEquals( trailingslashit( get_stylesheet_directory_uri() ) . static::SRC, $img->get_attribute( 'src' ) );
 	}
 
 	function test_width() {
-		$this->assertEquals( 1200, static::$img->get_width() );
+		$img = Image_Tag::create( static::SRC );
+		$this->assertEquals( 1200, $img->get_width() );
 	}
 
 	function test_height() {
-		$this->assertEquals( 769, static::$img->get_height() );
+		$img = Image_Tag::create( static::SRC );
+		$this->assertEquals( 769, $img->get_height() );
 	}
 
 	function test_ratio() {
-		$this->assertEquals( 0.640833333333333, static::$img->get_ratio() );
+		$img = Image_Tag::create( static::SRC );
+		$this->assertEquals( 0.640833333333333, $img->get_ratio() );
 	}
 
 }
