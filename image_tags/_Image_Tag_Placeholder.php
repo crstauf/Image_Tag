75b939a03c18abc01b43068cb137e69cad478ba0
Caleb Stauffer <develop+github@calebstauffer.com>
2020-04-13 18:18:09 -0400

    reset

---
+++ image_tags/_Image_Tag_Placeholder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Image tag generator for placeholder images.
+ */
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Abstract class: Image_Tag_Placeholder
+ */
+abstract class _Image_Tag_Placeholder extends Image_Tag {
+
+}
+
+?>
---
+++ image_tags/_Image_Tag_WP.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Image tag generator for WordPress images.
+ */
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Abstract class: Image_Tag_WP
+ */
+abstract class _Image_Tag_WP extends Image_Tag {
+
+}
+
+?>
