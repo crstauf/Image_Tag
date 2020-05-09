125bed00ec2640f11323d1f389e6312325902751
Caleb Stauffer <develop+github@calebstauffer.com>
2020-05-09 02:19:26 -0400

    add version check

--- tests/Image_Tag_Plugin_Test.php
+++ tests/Image_Tag_Plugin_Test.php
@@ -17,12 +17,27 @@ class Image_Tag_Plugin_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'https://develop.calebstauffer.com', $data['AuthorURI'] );
 		$this->assertEquals( 'Image Tag Generator', $data['Title'] );
 		$this->assertEquals( 'Caleb Stauffer', $data['AuthorName'] );
-		$this->assertEquals( '5.4', $data['RequiresWP'] );
-		$this->assertEquals( '7.3', $data['RequiresPHP'] );
 
 		$this->assertEmpty( $data['TextDomain'] );
 		$this->assertEmpty( $data['DomainPath'] );
 		$this->assertEmpty( $data['Network'] );
+	}
+
+	/**
+	 * "RequiresWP" and "RequiresPHP" indexes were added in 5.3.0.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/get_plugin_data/
+	 */
+	function test_requires_info() {
+		require ABSPATH . WPINC . '/version.php';
+
+		if ( version_compare( $wp_version, '5.3.0', '<=' ) )
+			$this->markTestSkipped( 'get_plugin_data() did not return "RequiresWP" and "RequiresPHP" prior to version 5.3.0' );
+
+		$data = get_plugin_data( dirname( __DIR__ ) . '/Image_Tag_Plugin.php', false, false );
+
+		$this->assertEquals( '5.4', $data['RequiresWP']  );
+		$this->assertEquals( '7.3', $data['RequiresPHP'] );
 	}
 
 	function test_constants() {
