013b745a8488e0776b3b083d2266e646a1357308
Caleb Stauffer <develop+github@calebstauffer.com>
2021-11-19 22:32:57 -0500

    test suite up and running!

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,4 +30,5 @@ function _manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
+require 'vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 require $_tests_dir . '/includes/bootstrap.php';
