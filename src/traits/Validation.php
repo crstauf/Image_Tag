764db93a5666cdf4c484f5c57ab489ec0c0297f7
Caleb Stauffer <develop+github@calebstauffer.com>
2025-06-14 21:02:13 -0400

    fix to not use static var

--- src/traits/Validation.php
+++ src/traits/Validation.php
@@ -4,28 +4,28 @@ namespace Image_Tag\Traits;
 
 trait Validation {
 
+	protected $valid = null;
+
 	public function is_valid() : bool {
 		return true === $this->check_valid();
 	}
 
 	protected function check_valid() : bool {
-		static $is_valid = null;
-
-		if ( ! is_null( $is_valid ) ) {
-			return $is_valid;
+		if ( ! is_null( $this->valid ) ) {
+			return $this->valid;
 		}
 
 		$errors = $this->perform_validation_checks();
 
 		if ( ! $errors->has_errors() ) {
-			return $is_valid = true;
+			return $this->valid = true;
 		}
 
 		foreach ( $errors->get_error_messages() as $message ) {
 			trigger_error( $message, E_USER_NOTICE );
 		}
 
-		return $is_valid = false;
+		return $this->valid = false;
 	}
 
 	abstract protected function perform_validation_checks() : \WP_Error;
